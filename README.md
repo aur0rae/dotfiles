53d6f743ec2763fc5eb8ea4865184b051a27d441
aur0rae <nbadovick@gmail.com>
2026-02-12 14:35:28 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# dotfiles
+# Dotfiles
 My current Sway dots!
+
+I'm using a fairly simple vanilla config of everything right now, but this should help minimize the 
+amount of trips I have to make to the Arch Wiki :3
+
+I'll attach pictures soon for reference
---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# This sucks but it's probably good enough for me
+
+sudo pacman -S --noconfirm sway swaybg swayidle swaylock wmenu brightnessctl foot dolphin \
+                           ark mpv yt-dlp mpd ncmpcpp bat eza fzf zoxide kdenlive obs-studio \
+                           prismlauncher steam breeze breeze-gtk breeze-icons
+
+cp -r config/* $HOME/.config/
+mkdir -p ~/.local/state/mpd/
+
+systemctl enable --user --now mpd.socket
+
